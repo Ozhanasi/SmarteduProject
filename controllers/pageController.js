bf97814643bc15a1f9888581816c6f9bef0d3aac
Özhan <ozhanasi01@gmail.com>
2022-12-09 19:29:11 +0300

    flash message added

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,4 +1,4 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
@@ -32,7 +32,9 @@ exports.getContactPage = (req, res) => {
 };
 
 exports.sendEmail = async (req, res) => {
-  
+
+  try{
+
   const outputMessage = `
   
   <h1>Mail Details</h1>
@@ -42,12 +44,12 @@ exports.sendEmail = async (req, res) => {
   </ul>
   <h1>Message</h1>
   <p> ${req.body.message}</p>
-  `
+  `;
 
   let testAccount = nodemailer.createTestAccount();
 
   // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
+  /*  let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true, // true for 465, false for other ports
@@ -56,22 +58,47 @@ exports.sendEmail = async (req, res) => {
       pass: "arxzycqbdrzqkcef", // gmail password
     },
   });
+*/
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'glennie.schiller@ethereal.email',
+      pass: 'jJTEe8sdUJuqvFEgCX',
+    },
+  });
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
+    from: '"Smart EDU Contact Form" <glennie.schiller@ethereal.email>', // sender address
+    to: 'glennie.schiller@ethereal.email', // list of receivers
+    subject: 'Smart EDU Contact Form New Message ✔', // Subject line
+    html: outputMessage, // html body
+  });
+  /*
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
     from: '"Smart EDU Contact Form" <muratkirpi01@gmail.com>', // sender address
-    to: "ozhanasi04@gmail.com", // list of receivers
+    to: "muratkirpi01@gmail.com", // list of receivers
     subject: "Smart EDU Contact Form New Message ✔", // Subject line
     html: outputMessage, // html body
   });
-
-  console.log("Message sent: %s", info.messageId);
+*/
+  console.log('Message sent: %s', info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
   // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
+  req.flash("success", "We Received your message successfully");
+
   res.status(200).redirect('contact');
+
+} catch (err) {
+  //req.flash("error", `Something happened! ${err}`);
+  req.flash("error", `Something happened!`);
+  res.status(200).redirect('contact');
+}
 
 };
